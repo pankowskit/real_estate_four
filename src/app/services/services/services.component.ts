39363a72bf2685c9fadd64691d8ec906a13625c0
pankowskitomasz <pankowski.email@gmail.com>
2022-02-28 19:38:56 +0200

    routing

---
+++ src/app/services/services/services.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-services',
+  templateUrl: './services.component.html',
+  styleUrls: [
+    './services.component.scss',
+    "../../sass/styles.scss"
+  ]
+})
+export class ServicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
